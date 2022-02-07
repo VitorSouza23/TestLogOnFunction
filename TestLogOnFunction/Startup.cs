c41a1a58f6fb6711ccc3ffda4ae995e6762d99c7
Vitor de Souza <vitordesolza@gmail.com>
2022-02-07 09:01:00 -0300

    First commit

---
+++ TestLogOnFunction/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(TestLogOnFunction.Startup))]
+namespace TestLogOnFunction;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ILoggerProvider>(sp =>
+        {
+            var functionDependencyContext = DependencyContext.Load(typeof(Startup).Assembly);
+            var hostConfig = sp.GetRequiredService<IConfiguration>();
+            var logger = new LoggerConfiguration()
+             .ReadFrom.Configuration(hostConfig, sectionName: "AzureFunctionsJobHost:Serilog", dependencyContext: functionDependencyContext)
+             .CreateLogger();
+
+            return new SerilogLoggerProvider(logger, dispose: true);
+        });
+    }
+
+}
